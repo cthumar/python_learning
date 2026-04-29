b2e7d1378d07250c90b33399fe2e8eabd0089faa
cthmr <chaitanya.thumar@festo.com>
2026-04-29 10:28:36 +0200

    rooting and exponentiation added

---
+++ my_path.py
@@ -0,0 +1,10 @@
+class my_math:
+    def exponentiation(self, base, exponent):
+        return base**exponent
+
+    def rooting(self, number, root):
+        return number ** (1 / root)
+
+
+if __name__ == "__main__":
+    my_math()
